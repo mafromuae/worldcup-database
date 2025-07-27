14c789fcc459f2e0ecbb8cc424acb09553b816ac
Mohamed Saeed <mohamed.saeed@email.com>
2025-07-27 14:56:11 -0400

    Initial commit of worldcup project

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,35 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+# Truncate tables before inserting new data
+echo $($PSQL "TRUNCATE games, teams;")
+
+# Read from games.csv
+cat games.csv | while IFS=',' read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  # Skip header row
+  if [[ $YEAR != "year" ]]
+  then
+    # Insert winner team if not exists
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER';")
+    if [[ -z $WINNER_ID ]]
+    then
+      echo $($PSQL "INSERT INTO teams(name) VALUES('$WINNER');")
+      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER';")
+    fi
+
+    # Insert opponent team if not exists
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT';")
+    if [[ -z $OPPONENT_ID ]]
+    then
+      echo $($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT');")
+      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT';")
+    fi
+
+    # Insert game row
+    echo $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals)
+      VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS);")
+  fi
+done
